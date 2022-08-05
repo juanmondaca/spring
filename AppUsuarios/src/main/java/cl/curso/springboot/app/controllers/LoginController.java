24dae3de3014bb7d62714d6a9c42ceff25abb15a
JotaM <juan.mondaca@gmail.com>
2022-08-05 16:33:38 -0400

    Modifica application.properties

--- AppUsuarios/src/main/java/cl/curso/springboot/app/controllers/LoginController.java
+++ AppUsuarios/src/main/java/cl/curso/springboot/app/controllers/LoginController.java
@@ -1,5 +1,6 @@
 package cl.curso.springboot.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import cl.curso.springboot.app.servicios.ILoginService;
+
 @Controller
 @RequestMapping("/auth")
 public class LoginController {
+	
+	// usar este atributo e inyeccion en la demo de servicios	
+	@Autowired
+	private ILoginService loginService;
+	
 	// llama al formulario de login
 	@GetMapping("/form-login")
 	public String loginUsuario() {
@@ -22,16 +30,23 @@ public class LoginController {
 			@RequestParam(value = "nombreUsuario") String l,
 			@RequestParam(value = "pwdUsuario") String pwd,
 			Model modelo) {
+		// 1. primera solucion: sin usar el servicio de Login
+		// verifica el usuario y la pwd contra valores fijos
+//		String nu = "usuario1";
+//		String pw = "Pwd1";
+//		
+//		if(l.trim().equalsIgnoreCase(nu) && pwd.trim().equals(pw)) {
+//			return "forward:/usuarios/listado";
+//		}
 		
-		// verifica el usurio y la pwd contra valores fijos
-		String nu = "usuario1";
-		String pw = "Pwd1";
-		
-		if(l.trim().equalsIgnoreCase(nu) && pwd.trim().equals(pw)) {
+		// 2. segunda solucion: usando el servicio de Login
+		// primero inyectar el servicio en un atributo del controlador
+		// segundo: usar el metodo de validacion
+		if(loginService.validaCredenciales(l, pwd)) {
 			return "forward:/usuarios/listado";
 		}
 		
-		return "forward:/";
+		return "forward:/"; //  redirect:https://www.google.com
 	}
 
 }
